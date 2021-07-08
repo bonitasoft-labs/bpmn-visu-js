1768683f8fd68df16723dfc17fabd285e212e1c0
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-07-08 11:58:09 +0200

    Update pages for use with the vite dev server

--- vite.config.js
+++ vite.config.js
@@ -13,3 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * @type {import('vite').UserConfig}
+ */
+const config = {
+  publicDir: 'dev/public',
+
+  // ...
+  server: {
+    port: 10001,
+  },
+};
+
+export default config;
